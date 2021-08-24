1e9b015f70b6ed213890f04d59e651163b33b34f
victoriagolynkina <victoriagolynkina@intersvyaz.ru>
2021-08-24 21:56:39 +0500

    Initial Commit

---
+++ fierbase-amm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fierbase-amm
+//
+//  Created by Вика on 24.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
